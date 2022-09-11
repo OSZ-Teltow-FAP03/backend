79ebc10e8475f0ae885a30ba9fdd6d10434cf6bd
Aland Mariwan <dev@aland-mariwan.de>
2022-09-11 08:53:37 +0200

    doc code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* This is importing the modules that we need to use in our application. */
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
@@ -7,6 +8,7 @@ var ping = require('ping');
 require('dotenv').config();
 const saltRounds = 10;
 
+/* This is creating a connection to the database. */
 var db = mysql.createConnection({
 	host: process.env.host,
 	user: process.env.user,
@@ -15,91 +17,99 @@ var db = mysql.createConnection({
 	database: process.env.database
 });
 
+/* This is creating a connection to the database. */
 db.connect(function(err) {
-	if (err) {
-		return console.error('error: ' + err.message);
-	}
-
+	if (err) return console.error('error: ' + err.message);
 	console.log('Connected to the MySQL server.');
 });
 
+/* This is a middleware that is used to parse the body of the request. */
 app.use(express.json());
 app.use(cors());
 
+/* This is a post request that is used to register a user. */
 app.post('/Register', (req, res) => {
+/* This is getting the data from the request body. */
 	const username = req.body.username;
 	const email = req.body.email;
 	const password = req.body.password;
 
+/* This is checking if the email is valid. */
 	if (!isEmail(email)) {
-		res.send({ msg: 'Invalid email' });
+		res.send({ msg: 'Invalid email', code: 101 });
 		return;
 	}
 
+/* This is checking if the username is valid. */
 	if (!checkUsername(username)) {
 		res.send({
 			msg:
-				'Username may only contain alphanumeric characters or single hyphens, and cannot begin or end with a hyphen.'
+				'Username may only contain alphanumeric characters or single hyphens, and cannot begin or end with a hyphen.',
+			code: 102
 		});
 		return;
 	}
 
+/* This is checking if the password is at least 8 characters long. */
 	if (password.length < 8) return res.send({ msg: 'Password must be at least 8 characters long.' });
 
-	db.query('SELECT * FROM users WHERE username = ?', [ username ], function(err, result) {
-		if (err) throw err;
-		if (result.length == 0) {
+/* This is checking if the username is already registered. */
+db.query('SELECT * FROM users WHERE username = ?', [ username ], function(err, result) {
+  if (err) throw err;
+  if (result.length == 0) {
+      /* This is checking if the Email is already registered. */
 			db.query('SELECT * FROM users WHERE email = ?', [ email ], function(err, result) {
 				if (err) throw err;
 				if (result.length == 0) {
 					bcrypt.hash(password, saltRounds, (err, hash) => {
-						db.query(
+						/* This is inserting the data into the database. */
+            db.query(
 							'INSERT INTO users (username, email, password) VALUE (?,?,?)',
 							[ username, email, hash ],
 							(error, response) => {
 								if (error) {
-									console.log('error :' + error);
-									res.send(error);
+									res.send({ msg: error });
 								} else if (err) {
-									console.log('err :' + err);
-									res.send(err);
+									res.send({ msg: err });
 								} else {
-									res.send({ msg: 'User successfully registered' });
+									res.send({ msg: 'User successfully registered', code: 201 });
 								}
 							}
 						);
 					});
 				} else {
-					res.send({ msg: 'Email already registered' });
+					res.send({ msg: 'Email already registered', code: 100 });
 				}
 			});
 		} else {
-			res.send({ msg: 'username already registered' });
+			res.send({ msg: 'username already registered', code: 100 });
 		}
 	});
 });
 
+/* This is a post request that is used to login a user. */
 app.post('/login', (req, res) => {
 	const email = req.body.email;
 	const password = req.body.password;
-	var userOrEmail = 'username';
+  var userOrEmail = 'username';
+  
+/* This is checking if the email or username. */
 	if (isEmail(email)) {
 		userOrEmail = 'email';
 	} else {
 		if (!checkUsername(email)) {
 			res.send({
 				msg:
-					'Username may only contain alphanumeric characters or single hyphens, and cannot begin or end with a hyphen.'
+					'Username may only contain alphanumeric characters or single hyphens, and cannot begin or end with a hyphen.',
+				code: 102
 			});
 			return;
 		}
 	}
 
-	console.log(email);
+/* This is checking if the user is registered. */
 	db.query('SELECT * FROM users WHERE ' + userOrEmail + ' = ?', [ email ], (err, result) => {
-		if (err) {
-			res.send(err);
-		}
+		if (err) res.send(err);
 		if (result.length > 0) {
 			bcrypt.compare(password, result[0].password, (error, response) => {
 				if (error) {
@@ -112,15 +122,20 @@ app.post('/login', (req, res) => {
 				if (response == true) {
 					res.send(response);
 				} else {
-					res.send({ msg: 'Email or password incorrect' });
+					res.send({ msg: 'Email or password incorrect', code: 105 });
 				}
 			});
 		} else {
-			res.send({ msg: 'Not registered user!' });
+			res.send({ msg: 'Not registered user!', code: 104 });
 		}
 	});
 });
 
+/**
+ * It checks if the email is valid or not
+ * @param email - The email address to validate.
+ * @returns A function that takes an email as an argument and returns true or false.
+ */
 let isEmail = (email) => {
 	// don't remember from where i copied this code, but this works.
 	let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -136,6 +151,11 @@ let isEmail = (email) => {
 	}
 };
 
+/**
+ * It checks if the username is valid or not
+ * @param username - The username to check.
+ * @returns A function that takes in a username and returns a boolean.
+ */
 let checkUsername = (username) => {
 	// don't remember from where i copied this code, but this works.
 	let re = /^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$/;
@@ -150,6 +170,7 @@ let checkUsername = (username) => {
 	}
 };
 
+/* This is telling the server to listen to port 3001. */
 app.listen(3001, () => {
 	console.log('running in the 3001');
 });
