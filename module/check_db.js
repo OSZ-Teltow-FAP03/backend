d4d1ff230c5d7488f69802d58b84da2a644aed35
Aland Mariwan <dev@aland-mariwan.de>
2022-09-29 15:01:34 +0200

    fix func checkDBTable

--- module/check_db.js
+++ module/check_db.js
@@ -1,7 +1,11 @@
-/* This is . */
+const db = require('../database/index');
+
 const checkDBTable = (table) => {
+	if (table == null || table.length === 0) return null;
 	db.query('SELECT * FROM ' + table, null, function(err, result) {
 		if (err) throw err.sqlMessage;
-		console.log(result);
+		return result;
 	});
 };
+
+module.exports = checkDBTable;
