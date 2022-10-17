5730b69ba7f5c90af701f573710527d089e18f2c
Aland Mariwan <dev@aland-mariwan.de>
2022-10-17 11:59:46 +0200

    merg AM

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
 /* This is importing the modules that we need to use in our application. */
+const express = require('express');
+require('./module/checkVersion');
+
+const helmet = require('helmet');
+const cors = require('cors'); //  A middleware that is used to parse the body of the request.
 const https = require('https');
 const fs = require('fs');
-const express = require('express');
+const errorHandlers = require('./handlers/errorHandlers');
+
 // Sessions can be stored server-side (ex: user auth) or client-side
 // (ex: shopping cart). express-session saves sessions in a store, and
 // NOT in a cookie. To store sessions in a cookie, use cookie-session.
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const cors = require('cors'); //  A middleware that is used to parse the body of the request.
-const app = express();
 require('dotenv').config();
-const SERVERPORT = process.env.SERVERPORT;
+const crypto = require('crypto');
+const nonce = crypto.randomBytes(16).toString('hex'); //#endregion
+
+// create our Express app
+const app = express();
+
+//setting CSP
+const csp = {
+	defaultSrc: [ `'none'` ],
+	styleSrc: [ `'self'`, `'unsafe-inline'` ],
+	scriptSrc: [ `'self'` ],
+	imgSrc: [ `'self'` ],
+	connectSrc: [ `'self'` ],
+	frameSrc: [ `'self'` ],
+	fontSrc: [ `'self'`, 'data:' ],
+	objectSrc: [ `'self'` ],
+	mediaSrc: [ `'self'` ],
+};
+
+//  app.use(helmet.noCache()); // noCache disabled by default
+const SERVERPORT = process.env.SERVERPORT || 4000;
 const SESSION_SECRET = process.env.SESSION_SECRET;
+const sixtyDaysInSeconds = 5184000; // 60 * 24 * 60 * 60
+
+// ======== *** SECURITY MIDDLEWARE ***
+
+//setup helmet js
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy(csp));
+app.use(helmet.hidePoweredBy());
+app.use(
+	helmet.hsts({
+		maxAge: sixtyDaysInSeconds,
+	}),
+);
 
 app.use(
 	cors({
@@ -20,9 +57,12 @@ app.use(
 		origin: true,
 	}),
 );
-app.set('trust proxy', 1); // trust first proxy
 
-//session middleware
+app.set('trust proxy', true); // trust first proxy
+app.disable('x-powered-by');
+
+// Sessions allow us to Contact data on visitors from request to request
+// This keeps admins logged in and allows us to send flash messages
 app.use(
 	session({
 		/* This is a secret key that is used to encrypt the session. */
@@ -63,7 +103,7 @@ app.use(
 		// See https://github.com/expressjs/session/issues/468
 		cookie: {
 			// Path attribute in Set-Cookie header. Defaults to the root path '/'.
-			// path: '/',
+			path: '/',
 
 			// Domain attribute in Set-Cookie header. There's no default, and
 			// most browsers will only apply the cookie to the current domain.
@@ -71,7 +111,7 @@ app.use(
 
 			// HttpOnly flag in Set-Cookie header. Specifies whether the cookie can
 			// only be read server-side, and not by JavaScript. Defaults to true.
-			// httpOnly: true,
+			httpOnly: true,
 
 			// Expires attribute in Set-Cookie header. Set with a Date object, though
 			// usually maxAge is used instead. There's no default, and the browsers will
@@ -80,8 +120,8 @@ app.use(
 
 			// Preferred way to set Expires attribute. Time in milliseconds until
 			// the expiry. There's no default, so the cookie is non-persistent.
-			maxAge: 1000 * 60 * 60 * 24, // Setting the cookie to expire in 24 hours.
-			// maxAge: 5 * 60 * 1000, // Setting the cookie to expire in 24 hours.
+			// maxAge: 1000 * 60 * 60 * 24, // Setting the cookie to expire in 24 hours.
+			maxAge: 1 * 60 * 1000,
 
 			// SameSite attribute in Set-Cookie header. Controls how cookies are sent
 			// with cross-site requests. Used to mitigate CSRF. Possible values are
@@ -91,9 +131,8 @@ app.use(
 
 			// Secure attribute in Set-Cookie header. Whether the cookie can ONLY be
 			// sent over HTTPS. Can be set to true, false, or 'auto'. Default is false.
-			// secure: false,
+			secure: true,
 			// HostOnly: true,
-			HttpOnly: true // This is a security feature that prevents the cookie from being accessed by JavaScript.
 		},
 	}),
 );
@@ -104,6 +143,7 @@ app.use(
 		extended: true,
 	}),
 );
+
 app.use(express.json());
 /* This is a middleware that is used to parse the body of the request. */
 const corsOptions = {
@@ -113,6 +153,7 @@ const corsOptions = {
 	optionsSuccessStatus: 200,
 	credentials: true,
 };
+
 app.use(cors(corsOptions));
 
 /*
@@ -134,43 +175,36 @@ app.use(cookieParser(SESSION_SECRET)); // any string ex: 'keyboard cat'
 const authRouter = require('./routes/auth');
 app.use('/auth', authRouter);
 
+const userRouter = require('./routes/userList');
+app.use('/auth', userRouter);
 
 
-app.use((req, res, next) => {
-
-	console.log(req.session);
-
-	// You can also access the cookie object above directly with
-	console.log(req.session.cookie);
-
-	// Beware that express-session only updates req.session on req.end(),
-	// so the values below are stale and will change after you read them
-	// (assuming that you roll sessions with resave and rolling).
-	console.log(req.session.cookie.expires); // date of expiry
-	console.log(req.session.cookie.maxAge); // milliseconds left until expiry
-
-	// Unless a valid session ID cookie is sent with the request,
-	// the session ID below will be different for each request.
-	console.log(req.session.id); // ex: VdXZfzlLRNOU4AegYhNdJhSEquIdnvE-
-
-	// Same as above. Alphanumeric ID that gets written to the cookie.
-	// It's also the SESSION_ID portion in 's:{SESSION_ID}.{SIGNATURE}'.
-	console.log(req.sessionID);
-
-	const { headers: { cookie } } = req;
-	if (cookie) {
-		const values = cookie.split(';').reduce((res, item) => {
-			const data = item.trim().split('=');
-			return {
-				...res,
-				[data[0]]: data[1],
-			};
-		}, {});
-		res.locals.cookie = values;
-		req.sessionID = values.session_id;
-	} else res.locals.cookie = {};
-	next();
+app.get('/', (req, res, next) => {
+	console.log(req.session)
+	if (req.session.user) {
+		res.status(200).send({
+			loggedIn: true,
+			user: req.session.user,
+		});
+	} else {
+		res.status(200).send({
+			loggedIn: false,
+		});
+	}
 });
+// pass variables to our templates + all requests
+
+// If that above routes didnt work, we 404 them and forward to error handler
+app.use(errorHandlers.notFound);
+
+// Otherwise this was a really bad error we didn't expect! Shoot eh
+if (app.get('env') === 'development') {
+	/* Development Error Handler - Prints stack trace */
+	app.use(errorHandlers.developmentErrors);
+}
+
+// production error handler
+app.use(errorHandlers.productionErrors);
 
 /* This is telling the server to listen to port 3001. */
 https
@@ -183,7 +217,7 @@ https
 		},
 		app,
 	)
-	.listen(SERVERPORT, (err) => {
+	.listen(SERVERPORT, '0.0.0.0', (err) => {
 		if (err) {
 			throw err;
 		} else {
