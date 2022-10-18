24a4d8fd3ccc44b53128a6d189684af384da7779
Martin <marxmymail1@gmail.com>
2022-10-18 08:43:21 +0200

    fully implemented

--- routes/films.js
+++ routes/films.js
@@ -4,20 +4,16 @@ const db = require("../database/index");
 
 router.get("/get", (req, res) => {
     if (req.session.user) {
-        console.log("Das ist in req.query drin");
-        console.log(req.query);
-        queryString = `SELECT * FROM Film`;
+        let queryString = `SELECT * FROM Film`;
 
         if (req.query.filmQuery !== undefined) {
-            queryString = `SELECT * FROM Film WHERE Filmtitel Like '%${req.query.stringsuche}%%' or Autor LIKE '%${req.query.stringsuche}%%' or Mitwirkende LIKE '%${req.query.stringsuche}%%' or Klasse like '%${req.query.stringsuche}%%' or Stichworte like '%${req.query.stringsuche}%%'`;
+            queryString = `SELECT * FROM Film WHERE Filmtitel Like '%${req.query.filmQuery}%' or Autor LIKE '%${req.query.filmQuery}%' or Mitwirkende LIKE '%${req.query.filmQuery}%' or Klasse like '%${req.query.filmQuery}%' or Stichworte like '%${req.query.filmQuery}%'`;
         }
 
-        if (true) {
-            db.query(queryString, function (err, result) {
-                if (err) throw err;
-                res.send(result);
-            });
-        }
+        db.query(queryString, function (err, result) {
+            if (err) throw err;
+            res.send(result);
+        });
     }
 });
 
