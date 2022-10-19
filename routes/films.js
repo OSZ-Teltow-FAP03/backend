df680a37d3e04c444704481418cbe86d89094e63
Kilian Spangenberg <spangenberg-kilian@t-online.de>
2022-10-19 11:04:22 +0200

    added changes in sql request

--- routes/films.js
+++ routes/films.js
@@ -10,11 +10,10 @@ router.get("/get", (req, res) => { //https://localhost:40324/films/get?filmQuery
         if (req.query.filmQuery !== undefined) {
             let filmQuery = `%${req.query.filmQuery}%`;
             let test =
-                db.query('SELECT * FROM Film WHERE Filmtitel Like ? or Autor LIKE ? or Mitwirkende LIKE ? or Klasse like ? or Stichworte like ?', [filmQuery, filmQuery, filmQuery, filmQuery, filmQuery], function (err, result) {
+                db.query("SELECT * FROM Film WHERE Filmtitel Like ? or Autor LIKE ? or Mitwirkende LIKE ? or Klasse like ? or Stichworte like ?", [filmQuery, filmQuery, filmQuery, filmQuery, filmQuery], function (err, result) {
                     if (err) throw err;
                     res.send(result);
                 });
-            console.log(test.sql);
         } else {
             db.query('SELECT * FROM Film', function (err, result) {
                 if (err) throw err;
