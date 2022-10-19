5f3b5c4bf78415f38272935205b558b845ebf4e4
Martin <marxmymail1@gmail.com>
2022-10-19 10:07:52 +0200

    pushed new changes

--- routes/films.js
+++ routes/films.js
@@ -3,20 +3,24 @@ const router = express.Router(); // Creating a router object.
 const db = require("../database/index");
 
 router.get("/get", (req, res) => { //https://localhost:40324/films/get?filmQuery={query}
-    if (req.session.user) {
+    if (true) {
 
+        console.log('im geting called')
         //when queryParam filmQuery is given (simple search)
         if (req.query.filmQuery !== undefined) {
-            db.query(`SELECT * FROM Film WHERE Filmtitel Like '%$?%' or Autor LIKE '%$?%' or Mitwirkende LIKE '%$?%' or Klasse like '%$?%' or Stichworte like '%?%'`, [filmQuery], function (err, result) {
+            let filmQuery = `%${req.query.filmQuery}%`;
+            let test =
+                db.query('SELECT * FROM Film WHERE Filmtitel Like ? or Autor LIKE ? or Mitwirkende LIKE ? or Klasse like ? or Stichworte like ?', [filmQuery, filmQuery, filmQuery, filmQuery, filmQuery], function (err, result) {
+                    if (err) throw err;
+                    res.send(result);
+                });
+            console.log(test.sql);
+        } else {
+            db.query('SELECT * FROM Film', function (err, result) {
                 if (err) throw err;
                 res.send(result);
             });
         }
-
-        db.query('SELECT * FROM Film', function (err, result) {
-            if (err) throw err;
-            res.send(result);
-        });
     }
 });
 
