1850471858553a8dd4de5610f212263a65eed7aa
Kilian Spangenberg <spangenberg-kilian@t-online.de>
2022-10-20 07:35:11 +0200

    Refactoring films

--- routes/films.js
+++ routes/films.js
@@ -3,17 +3,15 @@ const router = express.Router(); // Creating a router object.
 const db = require("../database/index");
 
 router.get("/get", (req, res) => { //https://localhost:40324/films/get?filmQuery={query}
-    if (true) {
+    if (req.session.user) {
 
-        console.log('im geting called')
         //when queryParam filmQuery is given (simple search)
         if (req.query.filmQuery !== undefined) {
             let filmQuery = `%${req.query.filmQuery}%`;
-            let test =
-                db.query("SELECT * FROM Film WHERE Filmtitel Like ? or Autor LIKE ? or Mitwirkende LIKE ? or Klasse like ? or Stichworte like ?", [filmQuery, filmQuery, filmQuery, filmQuery, filmQuery], function (err, result) {
-                    if (err) throw err;
-                    res.send(result);
-                });
+            db.query("SELECT * FROM Film WHERE Filmtitel Like ? or Autor LIKE ? or Mitwirkende LIKE ? or Klasse like ? or Stichworte like ?", [filmQuery, filmQuery, filmQuery, filmQuery, filmQuery], function (err, result) {
+                if (err) throw err;
+                res.send(result);
+            });
         } else {
             db.query('SELECT * FROM Film', function (err, result) {
                 if (err) throw err;
@@ -21,8 +19,7 @@ router.get("/get", (req, res) => { //https://localhost:40324/films/get?filmQuery
             });
         }
     }
-    else
-    {
+    else {
         res.status(400).send("not logged in")
         return
     }
