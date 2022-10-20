0b5bb347795536178609098cb604e16b957000a7
Pascal Hieke <igon3@hotmail.de>
2022-10-20 08:39:42 +0200

    Added Audio Support

--- routes/files.js
+++ routes/files.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router(); // Creating a router object.
 const db = require('../database/index');
 const fs = require('fs');
+const path = require('path');
 
 function checkFileExistsSync(filepath){
 	let flag = true;
@@ -23,34 +24,60 @@ router.get('/stream', (req, res) => {
 		res.status(400).send("Requires Range header");
 		return;
 	}
-	const FilmID=req.query.ID;
-	if(!FilmID){
-		res.status(400).send("ID not set");
+	console.log(req.query)
+	const FileID=req.query.FileID;
+	if(!FileID){
+		res.status(400).send("FileID not set");
 		return;
 	}
-	db.query('SELECT * FROM FilmDateien where FilmDateien.FilmID = ?', [FilmID], function (err, result) {
+	db.query('SELECT * FROM FilmDateien where FilmDateien.ID = ?', [FileID], function (err, result) {
 		if (err) throw err;
-		const videoPath = result[0].Dateipfad;
+		const filePath = result[0].Dateipfad;
 
-		if(!checkFileExistsSync(videoPath)){
+		if(!checkFileExistsSync(filePath)){
 			res.status(400).send("File not found");
 			return;
 		}
 
-		const videoSize = fs.statSync(videoPath).size;
+		const fileSize = fs.statSync(filePath).size;
+
+		const fileExtension=path.extname(filePath);
+		var contentType;
+		console.log(fileExtension)
+		console.log(filePath)
+		switch (fileExtension) {
+			case ".mp4":
+				contentType="video/mp4";
+				break;
+			case ".mp3":
+				contentType="audio/mpeg3";
+				break;
+			case ".wav":
+				contentType="audio/wav";
+				break;
+		
+			default:
+				break;
+		}
+
+		if(!contentType){
+			res.status(400).send("File not streamable");
+			return;
+		}
 
 		const CHUNK_SIZE = 10 ** 6; // 1MB
 		const start = Number(range.replace(/\D/g, ""));
-		const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+		const end = Math.min(start + CHUNK_SIZE, fileSize - 1);
 		const contentLength = end - start + 1;
 		const headers = {
-			"Content-Range": `bytes ${start}-${end}/${videoSize}`,
+			"Content-Range": `bytes ${start}-${end}/${fileSize}`,
 			"Accept-Ranges": "bytes",
 			"Content-Length": contentLength,
-			"Content-Type": "video/mp4",
+			"Content-Type": contentType,
 		};
+		
 		res.writeHead(206, headers);
-		const videoStream = fs.createReadStream(videoPath, { start, end });
+		const videoStream = fs.createReadStream(filePath, { start, end });
 		videoStream.pipe(res);
 	});
 });
@@ -60,12 +87,12 @@ router.get('/download', (req, res) => {
 		res.status(400).send("Not logged in");
 		return;
 	}
-	const FilmID=req.query.ID;
-	if(!FilmID){
-		res.status(400).send("ID not set");
+	const FileID=req.query.FileID;
+	if(!FileID){
+		res.status(400).send("FileID not set");
 		return;
 	}
-	db.query('SELECT * FROM FilmDateien where FilmDateien.FilmID = ?', [FilmID], function (err, result) {
+	db.query('SELECT * FROM FilmDateien where FilmDateien.ID = ?', [FileID], function (err, result) {
 		if (err) throw err;
 		const videoPath = result[0].Dateipfad;
 
