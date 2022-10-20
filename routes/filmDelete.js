2d54287c1f5d2cf5d8cd9ac8279875542c3aac66
Shojo-Shinji <baade3110@gmail.com>
2022-10-20 12:12:41 +0200

    Filmdateien werde jetzt auch gelöscht (aus table filmDateien)

--- routes/filmDelete.js
+++ routes/filmDelete.js
@@ -6,13 +6,16 @@ const express = require('express');
 const router = express.Router(); // Creating a router object.
 const db = require('../database/index');
 
-/* This is a post request that is used to delete a film from db. */
+/* This is a post request that is used to delete a film from db. */ 
+/* test: https://localhost:40324/filmDelete/delete (json: {"filmID":"1"}) */
 router.delete('/delete', (req, res) => {
 	/* This is getting the data from the request body. */
 	let filmID = decrypt(req.body.filmID);
 
-	/* Trying to delete the film */		//test: https://localhost:40324/filmDelete/delete (body: filmID=x)
+	/* Trying to delete the film */	
 	db.query('DELETE FROM Film WHERE ID = ?',[ filmID ],(error, response) => {
+		/* delete Filmdateien */
+		db.query('DELETE FROM FilmDateien WHERE FilmID = ?',[ filmID ],(error, response) => {
 			if (error) {
 				res.send({
 					msg: error
@@ -24,9 +27,8 @@ router.delete('/delete', (req, res) => {
 					code: 200
 				});
 			}
-		}
-	);				
-});
-
+		});
+	});		
+})
 /* This is exporting the router object. */
 module.exports = router;
