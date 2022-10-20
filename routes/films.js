7a49ef99c8a8380372269e81e87b70bb919dfbf5
marxmy <30389083+marxmy@users.noreply.github.com>
2022-10-20 07:39:30 +0200

    Feature/films  (#15)
    * add films route
    
    * fixed errors in main
    
    * add seesion user check
    
    * added sql statement
    
    * fully implemented
    
    * added example route
    
    * using sql injection protection
    
    * added films
    
    * pushed new changes
    
    * added changes in sql request
    
    * Refactoring films
    
    Co-authored-by: Kilian Spangenberg <spangenberg-kilian@t-online.de>

---
+++ routes/films.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router(); // Creating a router object.
+const db = require("../database/index");
+
+router.get("/get", (req, res) => { //https://localhost:40324/films/get?filmQuery={query}
+    if (req.session.user) {
+
+        //when queryParam filmQuery is given (simple search)
+        if (req.query.filmQuery !== undefined) {
+            let filmQuery = `%${req.query.filmQuery}%`;
+            db.query("SELECT * FROM Film WHERE Filmtitel Like ? or Autor LIKE ? or Mitwirkende LIKE ? or Klasse like ? or Stichworte like ?", [filmQuery, filmQuery, filmQuery, filmQuery, filmQuery], function (err, result) {
+                if (err) throw err;
+                res.send(result);
+            });
+        } else {
+            db.query('SELECT * FROM Film', function (err, result) {
+                if (err) throw err;
+                res.send(result);
+            });
+        }
+    }
+    else {
+        res.status(400).send("not logged in")
+        return
+    }
+});
+
+/* This is exporting the router object. */
+module.exports = router;
