322567fc6d5d0e8f11a365f74c4e39181cfc4318
Shojo-Shinji <97019278+Shojo-Shinji@users.noreply.github.com>
2022-11-23 14:12:57 +0100

    Film delete (#29)
    Film delete Endpoint

--- routes/films.js
+++ routes/films.js
@@ -45,6 +45,57 @@ router.get("/listFiles", (req, res) => {
     });
 });
 
+/* This is a post request that is used to delete a film from db. */ 
+/* test: https://localhost:40324/filmDelete/delete (json: {"filmID":"1"}) */
+router.delete('/delete', (req, res) => {
+	/* check if user is logged in */
+	if(!req.session.user){
+		res.status(400).send("Not logged in");
+		return;
+	}
+
+	const filmID = decrypt(req.body.filmID);
+	const prüfstück = decrypt(req.body.Prüfstück)
+	const rights = false;
+	switch (req.session.user.role) {
+		case "admin":
+			rights = true;
+			break;
+		case "pruefer":
+			if(prüfstück) rights = true;
+			break;
+		case "lehrerMedien":
+			if(!prüfstück) rights = true;
+			break;
+		default:
+			break;
+	}
+	if (!rights) return res.status(400).send("Not enough privileges"); 
+
+	/* Trying to delete the film */	
+	db.query('DELETE FROM Film WHERE ID = ?',[ filmID ],(error, response) => {
+		if (error) {
+			res.send({
+				msg: error
+			});
+		} else {
+			/* delete Filmdateien */
+			db.query('DELETE FROM FilmDateien WHERE FilmID = ?',[ filmID ],(error2, response2) => {
+				if (error2) {
+					res.send({
+						msg: error2
+					});
+				} else {
+					res.send({
+						/* Film got deleted */
+						msg: 'Film successfully deleted',
+						code: 200
+					});
+				}
+			});
+		}
+	});		
+})
 /* It's a mess. */
 router.post('/create', (req, res) => {
     if (!req.session.user) return res.status(400).send("Not logged in");
