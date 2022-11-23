7d119ffe271d9d75ab7e95b3e6e3789396f44f03
Kilian Spangenberg <spangenberg-kilian@t-online.de>
2022-11-23 14:45:31 +0100

    added iteration over request body

--- routes/films.js
+++ routes/films.js
@@ -32,7 +32,7 @@ router.get("/get", (req, res) => { //https://localhost:40324/films/get?filmQuery
 
 router.patch("/patch", (req, res) => {//https://localhost:40324/films/patch
     /*{
-    "FilmId": "9",
+    "ID": "9",
     "Filmtitel": "Filmtitel",
     "Tonformat": "Tonformat",
     "Bildformat": "Bild",
@@ -40,13 +40,13 @@ router.patch("/patch", (req, res) => {//https://localhost:40324/films/patch
     "Farbtiefe": "Farbtiefe",
     "Videocontainer": "Videocontainer",
     "Tonspurbelegung": "A8",
-    "TimecodeAnfang": "00:00:00",
-    "TimecodeEnde": "00:00:00",
+    "Timecode_Anfang": "00:00:00",
+    "Timecode_Ende": "00:00:00",
     "Dauer": "00:00:00",
     "Videocodec": "Videocodec",
     "Auflösung": "Auflösung",
     "Vorschaubild": "Vorschaubild",
-    "Erscheinungdsdatum": "1999-02-15",
+    "Erscheinungsdatum": "1999-02-15",
     "Autor": "Autor",
     "Programmtyp": "Programmtyp",
     "Erzählsatz": "Erzählsatz",
@@ -59,173 +59,34 @@ router.patch("/patch", (req, res) => {//https://localhost:40324/films/patch
     "Status": "Status",
     "Lehrjahr": "1999999",
     "Stichworte": "Stichworte"
-    }*/
-    if (req.session.user == "admin" || req.session.user == "prüfungskommission") {
-        const Filmtitel = decrypt(req.body.Filmtitel);
-        const Tonformat = decrypt(req.body.Tonformat);
-        const Bildformat = decrypt(req.body.Bildformat);
-        const Bildfrequenz = decrypt(req.body.Bildfrequenz);
-        const Farbtiefe = decrypt(req.body.Farbtiefe);
-        const Videocontainer = decrypt(req.body.Videocontainer);
-        const Tonspurbelegung = decrypt(req.body.Tonspurbelegung);
-        const TimecodeAnfang = decrypt(req.body.TimecodeAnfang);
-        const TimecodeEnde = decrypt(req.body.TimecodeEnde);
-        const Dauer = decrypt(req.body.Dauer);
-        const Videocodec = decrypt(req.body.Videocodec);
-        const Auflösung = decrypt(req.body.Auflösung);
-        const Vorschaubild = decrypt(req.body.Vorschaubild);
-        const Erscheinungsdatum = decrypt(req.body.Erscheinungdsdatum);
-        const Autor = decrypt(req.body.Autor);
-        const Programmtyp = decrypt(req.body.Programmtyp);
-        const Erzählsatz = decrypt(req.body.Erzählsatz);
-        const Bemerkung = decrypt(req.body.Bemerkung);
-        const Erstellungsdatum = decrypt(req.body.Erstellungsdatum);
-        const Mitwirkende = decrypt(req.body.Mitwirkende);
-        const Bewertungen = decrypt(req.body.Bewertungen);
-        const Upload = decrypt(req.body.Upload);
-        const Klasse = decrypt(req.body.Klasse);
-        const Status = decrypt(req.body.Status);
-        const Lehrjahr = decrypt(req.body.Lehrjahr);
-        const Stichworte = decrypt(req.body.Stichworte);
-        const FilmId = decrypt(req.body.FilmId);
+}*/
+    if (req.session.user == "admin" || req.session.user == "lehrer"|| req.session.user == "pruefer" || true){
+
+        const FilmId = decrypt(req.body.ID);
+        const prüfstück = null;
+        db.query("SELECT Prüfstück FROM Film WHERE ID = ?",[FilmId], function (err, result) {
+            if (err) throw err;
+            prüfstück = result;
+            console.log(prüfstück);
+        });
+
+        if (condition) {
+            
+        }
 
         let arrayOfValues = []
-        let isUpdate = false;
-
         let updateQuery = 'UPDATE Film SET ';
-        if (Filmtitel != null) {
-            arrayOfValues.push(Filmtitel);
-            updateQuery += 'Filmtitel = ?,';
-            isUpdate = true;
-        }
-        if (Tonformat != null) {
-            arrayOfValues.push(Tonformat);
-            updateQuery += 'Tonformat = ?,';
-            isUpdate = true;
-        }
-        if (Bildformat != null) {
-            arrayOfValues.push(Bildformat);
-            updateQuery += 'Bildformat = ?,';
-            isUpdate = true;
-        }
-        if (Bildfrequenz != null) {
-            arrayOfValues.push(Bildfrequenz);
-            updateQuery += 'Bildfrequenz = ?,';
-            isUpdate = true;
-        }
-        if (Farbtiefe != null) {
-            arrayOfValues.push(Farbtiefe);
-            updateQuery += 'Farbtiefe = ?,';
-            isUpdate = true;
-        }
-        if (Videocontainer != null) {
-            arrayOfValues.push(Videocontainer);
-            updateQuery += 'Videocontainer = ?,';
-            isUpdate = true;
-        }
-        if (Tonspurbelegung != null) {
-            arrayOfValues.push(Tonspurbelegung);
-            updateQuery += 'Tonspurbelegung = ?,';
-            isUpdate = true;
-        }
-        if (TimecodeAnfang != null) {
-            arrayOfValues.push(TimecodeAnfang);
-            updateQuery += 'Timecode_Anfang = ?,';
-            isUpdate = true;
-        }
-        if (TimecodeEnde != null) {
-            arrayOfValues.push(TimecodeEnde);
-            updateQuery += 'Timecode_Ende = ?,';
-            isUpdate = true;
-        }
-        if (Dauer != null) {
-            arrayOfValues.push(Dauer);
-            updateQuery += 'Dauer = ?,';
-            isUpdate = true;
-        }
-        if (Videocodec != null) {
-            arrayOfValues.push(Videocodec);
-            updateQuery += 'Videocodec = ?,';
-            isUpdate = true;
-        }
-        if (Auflösung != null) {
-            arrayOfValues.push(Auflösung);
-            updateQuery += 'Auflösung = ?,';
-            isUpdate = true;
-        }
-        if (Vorschaubild != null) {
-            arrayOfValues.push(Vorschaubild);
-            updateQuery += 'Vorschaubild = ?,';
-            isUpdate = true;
-        }
-        if (Erscheinungsdatum != null) {
-            arrayOfValues.push(Erscheinungsdatum);
-            updateQuery += 'Erscheinungsdatum = ?,';
-            isUpdate = true;
-        }
-        if (Autor != null) {
-            arrayOfValues.push(Autor);
-            updateQuery += 'Autor = ?,';
-            isUpdate = true;
-        }
-        if (Programmtyp != null) {
-            arrayOfValues.push(Programmtyp);
-            updateQuery += 'Programmtyp = ?,';
-            isUpdate = true;
-        }
-        if (Erzählsatz != null) {
-            arrayOfValues.push(Erzählsatz);
-            updateQuery += 'Erzählsatz = ?,';
-            isUpdate = true;
-        }
-        if (Bemerkung != null) {
-            arrayOfValues.push(Bemerkung);
-            updateQuery += 'Bemerkung = ?,';
-            isUpdate = true;
-        }
-        if (Erstellungsdatum != null) {
-            arrayOfValues.push(Erstellungsdatum);
-            updateQuery += 'Erstellungsdatum = ?,';
-            isUpdate = true;
-        }
-        if (Mitwirkende != null) {
-            arrayOfValues.push(Mitwirkende);
-            updateQuery += 'Mitwirkende = ?,';
-            isUpdate = true;
-        }
-        if (Bewertungen != null) {
-            arrayOfValues.push(Bewertungen);
-            updateQuery += 'Bewertungen = ?,';
-            isUpdate = true;
-        }
-        if (Upload != null) {
-            arrayOfValues.push(Upload);
-            updateQuery += 'Upload = ?,';
-            isUpdate = true;
-        }
-        if (Klasse != null) {
-            arrayOfValues.push(Klasse);
-            updateQuery += 'Klasse = ?,';
-            isUpdate = true;
-        }
-        if (Status != null) {
-            arrayOfValues.push(Status);
-            updateQuery += 'Status = ?,';
-            isUpdate = true;
-        }
-        if (Lehrjahr != null) {
-            arrayOfValues.push(Lehrjahr);
-            updateQuery += 'Lehrjahr = ?,';
-            isUpdate = true;
-        }
-        if (Stichworte != null) {
-            arrayOfValues.push(Stichworte);
-            updateQuery += 'Stichworte = ?,';
-            isUpdate = true;
-        }
+        
+        //iterating over req body to dynamically enter attribute names to sql query
+        Object.entries(req.body).forEach(entry => {
+            const [key, value] = entry;
+            arrayOfValues.push(value);
+            updateQuery += key + ' = ?,';
+            //console.log(key, value);
+          });
 
         //When no param is recognised in body then nothing is changed
-        if (!isUpdate) {
+        if (arrayOfValues.length == 0) {
             res.status(400).send('Nothing to update.');
             return;
         }
