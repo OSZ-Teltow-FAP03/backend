db40d0762c448ac076f189e4ec977839b70b089b
igon3 <112931105+igon3@users.noreply.github.com>
2022-12-15 10:12:17 +0100

    Update users.js
    Signed-off-by: igon3 <112931105+igon3@users.noreply.github.com>

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const { encrypt, decrypt } = require('../modules/crpyto');
 const express = require('express');
 const router = express.Router(); // Creating a router object.
 const db = require('../database/index');
+const { checkPrivileges } = require('../modules/check_privileges');
 
 router.get('/get', (req, res) => {
 	if(!req.session.user){
@@ -11,6 +12,15 @@ router.get('/get', (req, res) => {
 		});
 		return;
 	}
+	
+	if(!checkPrivileges(req.baseUrl+req.path, req.session.user.role)){
+		res.status(400).send({
+			msg: 'Missing privileges',
+			code: 113
+		});
+		return;
+	}
+	
 	const userID = req.query.userID;
 	if(userID===false){
 		res.status(400).send({
@@ -45,7 +55,7 @@ router.patch('/updateRole', (req, res) => {
 		return;
 	}
 	const userID = decrypt(req.body.userID);
-	let role = decrypt(req.body.role);
+	const role = decrypt(req.body.role);
 	if(userID===false || role===false){
 		res.status(400).send({
 			msg: 'Request not valid',
@@ -53,8 +63,8 @@ router.patch('/updateRole', (req, res) => {
 		});
 		return;
 	}
-	role=role.toLowerCase();
-	db.query('UPDATE users SET role = ? WHERE userID = ?', [ role, userID ], (err, result) => {
+	
+	db.query('SELECT role FROM users WHERE userID = ?', [ userID ], (err, result) => {
 		if (err){
 			throw res.status(500).send({
 				msg: err,
@@ -62,9 +72,35 @@ router.patch('/updateRole', (req, res) => {
 			});
 			return;
 		}
-		res.send({
-			msg: "User updated",
-			code: 205
+		
+		if(result.length!==1){
+			res.status(400).send({
+				msg: 'User not found',
+				code: 117
+			});
+			return;
+		}
+		
+		if(!checkPrivileges(req.baseUrl+req.path, req.session.user.role, false, {newRole: role, oldRole: result[0].role})){
+			res.status(400).send({
+				msg: 'Missing privileges',
+				code: 113
+			});
+			return;
+		}
+
+		db.query('UPDATE users SET role = ? WHERE userID = ?', [ role, userID ], (err2, result2) => {
+			if (err2){
+				throw res.status(500).send({
+					msg: err2,
+					code: 402
+				});
+				return;
+			}
+			res.send({
+				msg: "User updated",
+				code: 205
+			});
 		});
 	});
 });
@@ -77,6 +113,15 @@ router.get('/list', (req, res) => {
 		});
 		return;
 	}
+	
+	if(!checkPrivileges(req.baseUrl+req.path, req.session.user.role)){
+		res.status(400).send({
+			msg: 'Missing privileges',
+			code: 113
+		});
+		return;
+	}
+	
 	db.query('SELECT username, name, lastname, email, role FROM users', function(err, result) {
 		if (err){
 			throw res.status(500).send({
@@ -101,6 +146,15 @@ router.delete('/delete', (req, res) => {
 		});
 		return;
 	}
+	
+	if(!checkPrivileges(req.baseUrl+req.path, req.session.user.role)){
+		res.status(400).send({
+			msg: 'Missing privileges',
+			code: 113
+		});
+		return;
+	}
+	
 	const userID = decrypt(req.body.userID);
 	if(userID===false){
 		res.status(400).send({
@@ -109,6 +163,7 @@ router.delete('/delete', (req, res) => {
 		});
 		return;
 	}
+	
 	db.query('DELETE FROM users WHERE userID = ?', [ userID ], (err, result) => {
 		if (err){
 			throw res.status(500).send({
